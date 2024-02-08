7733e4067743e3da2e7b57fdc3745e3e7e1b1fff
realj <realjesseallen@gmail.com>
2024-02-08 15:30:22 -0500

    True turn-based server, no bugs. Modularized client

--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -4,13 +4,124 @@
 #include <iostream>
 #include <thread>
 
-void receiveMessages(sf::TcpSocket& socket, sf::CircleShape& shape) {
+class Client {
+    std::string identity;
+    std::string playerTurn;
+
+
+public:
+    Client() {}
+
+    void initailize(sf::TcpSocket& socket) {
+
+        // set server IP address and port number
+        //sf::IpAddress ip = "127.0.0.1";
+        sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+        int port = 2222;
+
+        // Connect to the server
+
+        if (socket.connect(ip, 2222) != sf::Socket::Done) {
+            std::cerr << "Error Failed to connect to the server" <<  std::endl;
+            // handle error
+        }
+
+
+        // Receive identity from the server
+        sf::Packet identityPacket;
+        if (socket.receive(identityPacket) != sf::Socket::Done) {
+            std::cerr << "Error: Failed to receive identity from the server" << std::endl;
+            //handle error
+        }
+
+        identityPacket >> identity >> playerTurn;
+        std::cout << "You are " << identity << std::endl;
+        std::cout << "player turn: " << playerTurn << std::endl;
+
+    }
+
+    std::string getIdentity() {
+        return identity;
+    }
+
+    std::string getPlayerTurn() {
+        return playerTurn;
+    }
+
+    void setPlayerTurn(std::string turn) {
+        playerTurn = turn;
+    }
+
+    void sendCircleColorPacket(sf::CircleShape& shape, sf::TcpSocket& socket) {
+
+        //create packet to send
+        sf::Packet packet;
+
+        // Create a message packet and send it to the server
+        // We can use the Packet class to build blocks of data to transfer over the network
+        // we can send a node position, for example position 10
+        // and a coalition, for example green
+        // packet << 10 << "green"; 
+
+        //change circle color
+        //send mesage to server to request element change and wait for a response
+        if (shape.getFillColor() == sf::Color::Blue) {
+            packet << "green";
+        }
+
+        else {
+            packet << "blue";
+        }
+
+        // send packet to server
+        if (socket.send(packet) != sf::Socket::Done) {
+            std::cerr << "Error: Failed to send packet to the server" << std::endl;
+            //handle error accordingly
+        }
+    }
+};
+
+class Rendering {
+
+public:
+
+    Rendering() {}
+
+    void initializeWindow(sf::RenderWindow& window) {}
+};
+
+class Input {
+
+public:
+
+    Input() {}
+
+    bool mouseClickOnShape(sf::Event& event, sf::CircleShape& shape, sf::RenderWindow& window) {
+
+        if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+            sf::Vector2f mousePosition = window.mapPixelToCoords(sf::Vector2i(event.mouseButton.x, event.mouseButton.y));
+            //check if mouse click is in shape
+            if (shape.getGlobalBounds().contains(mousePosition))
+                return true;
+            else
+                return false;
+        }
+        return false;
+    }
+};
+
+void receiveMessages(sf::TcpSocket& socket, sf::CircleShape& shape, Client& client) {
     while (true) {
         sf::Packet receivedPacket;
         if (socket.receive(receivedPacket) == sf::Socket::Done) {
-            std::string s;
-            receivedPacket >> s;
-            if (s == "blue") {
+            std::string color;
+            std::string playerTurn;
+            receivedPacket >> color >> playerTurn;
+
+            std::cout << "player turn: " << playerTurn << std::endl;
+            client.setPlayerTurn(playerTurn);
+
+            if (color == "blue") {
                 shape.setFillColor(sf::Color::Blue);
             }
             else {
@@ -20,43 +131,31 @@ void receiveMessages(sf::TcpSocket& socket, sf::CircleShape& shape) {
     }
 }
 
-
 int main() {
-    // set server IP address and port number
-    sf::IpAddress ip = "192.168.19.131";
-    //sf::IpAddress ip = sf::IpAddress::getLocalAddress();
-    int port = 2222;
 
-    // Connect to the server
+    //create client object and initialize with socket
+    Client client;
     sf::TcpSocket socket;
-    if (socket.connect(ip, 2222) != sf::Socket::Done) {
-        std::cerr << "Error: Failed to connect to the server" << std::endl;
-        return 1;
-    }
+    client.initailize(socket);
 
-    // Receive identity from the server
-    sf::Packet identityPacket;
-    if (socket.receive(identityPacket) != sf::Socket::Done) {
-        std::cerr << "Error: Failed to receive identity from the server" << std::endl;
-        return 1;
-    }
+    //create rendering object
+    Rendering rendering;
 
-    std::string identity;
-    
-    identityPacket >> identity;
-    
-    std::cout << "Server says: " << identity << std::endl;
+    //setup window and initial shapes
+    sf::RenderWindow window(sf::VideoMode(200, 200), "TopoTactics!");
+    //rendering.initializeWindow(window);
 
-    // setup window and initial shape color
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    //setup game elements
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    // Start the message receiving thread
-    std::thread receiveThread(receiveMessages, std::ref(socket), std::ref(shape));
+    //create Input object
+    Input input;
 
+    //Start the message receiving thread
+    std::thread receiveThread(receiveMessages, std::ref(socket), std::ref(shape), std::ref(client));
 
-    // Game loop
+    //Game loop
     while (window.isOpen()) {
 
         sf::Event event;
@@ -65,102 +164,21 @@ int main() {
                 window.close();
             }
 
-            // if player 1's turn, handle mouse click
-            if (identity == "You are player 1") {
-                if (event.type == sf::Event::MouseButtonPressed) {
-                    if (event.mouseButton.button == sf::Mouse::Left) {
-                        sf::Vector2f mousePosition = window.mapPixelToCoords(sf::Vector2i(event.mouseButton.x, event.mouseButton.y));
-
-                        // check if mouse click is in shape
-                        if (shape.getGlobalBounds().contains(mousePosition)) {
-
-                            // create packet to send 
-                            sf::Packet packet;
-
-                            // change circle color
-                            // ??? send mesage to server to request element change and wait for a response???
-                            if (shape.getFillColor() == sf::Color::Blue) {
-                                //shape.setFillColor(sf::Color::Green); // dont set fill color here
-
-                                packet << "green";
-                            }
-
-                            else {
-                                //shape.setFillColor(sf::Color::Blue); // dont set fill color here
-                                packet << "blue";
-                            }
-
-                            // send packet to server
-                            if (socket.send(packet) != sf::Socket::Done) {
-                                std::cerr << "Error: Failed to send packet to the server" << std::endl;
-                                // handle error accordingly
-                                break;
-                            }
-
-                        }
-                    }
+            // check if player's turn 
+            if (client.getIdentity() == client.getPlayerTurn()) {
+                //check if mouse click is on a shape
+                if (input.mouseClickOnShape(event, shape, window)) {
+                    client.sendCircleColorPacket(shape, socket);
                 }
-            }
-
-
-            // If it's player 2's turn, handle mouse click
-            if (identity == "You are player 2") {
-
-                if (event.type == sf::Event::MouseButtonPressed) {
-                    if (event.mouseButton.button == sf::Mouse::Left) {
-                        sf::Vector2f mousePosition = window.mapPixelToCoords(sf::Vector2i(event.mouseButton.x, event.mouseButton.y));
-
-                        if (shape.getGlobalBounds().contains(mousePosition)) {
-
-                            /* 
-                            * create packet to send to the server
-                            * We will use the Packet class to build blocks of data to transfer over the network 
-                            * we can send a node position, for example position 10
-                            * and a coalition, for example green
-                            * sf::Uint32 x = 10; 
-					        * string s = "green";
-                            * packet << x << s;
-                            */
-                            sf::Packet packet;
-                            // change circle color
-                            if (shape.getFillColor() == sf::Color::Blue) {
-
-                                packet << "green";
-                            }
-
-                            else {
-                                packet << "blue";
-                            }
-
-                            // send packet to server
-                            if (socket.send(packet) != sf::Socket::Done) {
-                                std::cerr << "Error: Failed to send packet to the server" << std::endl;
-                                // handle error accordingly
-                                break;
-                            }
-
-
-                        }
-
-                    }
-
-
-                }
-
-
-            }
-
+            }  
         }
-
+ 
         window.clear();
-        // Draw game elements
         window.draw(shape);
         window.display();
-
-
     }
 
-    // Disconnect from the server
+    //Disconnect from the server
     receiveThread.join();
     socket.disconnect();
 
--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -33,16 +33,23 @@ int main() {
 
     std::cout << "Player 2 connected" << std::endl;
 
-    // Assign player identities
+    // Assign player identities and player turn packets
+    std::string playerIdentity = "Player1";
+    std::string playerTurn = "Player1";
+
     sf::Packet identityPacket;
-    identityPacket << "You are player 1";
+    identityPacket << playerIdentity << playerTurn;
+
+
     if (player1.send(identityPacket) != sf::Socket::Done) {
         std::cerr << "Error: Failed to send identity to player 1" << std::endl;
         return 1;
     }
 
+    playerIdentity = "Player2";
     identityPacket.clear();
-    identityPacket << "You are player 2";
+    identityPacket << playerIdentity << playerTurn;
+
     if (player2.send(identityPacket) != sf::Socket::Done) {
         std::cerr << "Error: Failed to send identity to player 2" << std::endl;
         return 1;
@@ -52,14 +59,26 @@ int main() {
     while (true) {
         // Receive message from player 1
         sf::Packet packet1;
-        if (player1.receive(packet1) != sf::Socket::Done) {
+        sf::Socket::Status player1Status = player1.receive(packet1);
+        if (player1Status != sf::Socket::Done) {
             std::cerr << "Error: Failed to receive message from player 1" << std::endl;
             break;
+        }
+        else if (player1Status == sf::Socket::Done) {
+       
+            std::string s;
+            packet1 >> s;
+            packet1.clear();
+
+            std::cout << "Received message from player 1: " << s << std::endl;
+
+            playerTurn = "Player2";
+            packet1 << s << playerTurn;
+            
         }
 
         // 
         // Packet received form client 1, send packet back to player 1 and player 2 to set the fill color
-        // 
         // a)
         // Forward message to player 2
         if (player2.send(packet1) != sf::Socket::Done) {
@@ -67,9 +86,9 @@ int main() {
             break;
         }
 
-        // also send packet to player 1
         // b)
-        
+        // also send packet back to player 1
+
         if (player1.send(packet1) != sf::Socket::Done) {
             std::cerr << "Error: Failed to send message to player 2" << std::endl;
             break;
@@ -78,10 +97,23 @@ int main() {
 
         // Receive message from player 2
         sf::Packet packet2;
-        if (player2.receive(packet2) != sf::Socket::Done) {
+        sf::Socket::Status player2Status = player2.receive(packet2);
+        if (player2Status != sf::Socket::Done) {
             std::cerr << "Error: Failed to receive message from player 2" << std::endl;
             break;
         }
+        else if (player2Status == sf::Socket::Done) {
+            
+            std::string s;
+            packet2 >> s;
+            packet2.clear();
+
+            std::cout << "Received message from player 2: " << s << std::endl;
+
+            playerTurn = "Player1";
+            packet2 << s << playerTurn;
+        }
+
         // 
         // Packet received from client 2, send packet back to player 1 and player 2 to set the fill color
         // 
@@ -91,15 +123,14 @@ int main() {
             std::cerr << "Error: Failed to send message to player 1" << std::endl;
             break;
         }
+
         // b)
-        
+        // also send packet back to player 2
         if (player2.send(packet2) != sf::Socket::Done) {
             std::cerr << "Error: Failed to send message to player 1" << std::endl;
             break;
         }
         
-
-
     }
 
     // Close connections
