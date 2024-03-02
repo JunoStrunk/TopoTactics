28d3bf3bc6c4882173d3ff2f3604c706e8dc427a
realj <realjesseallen@gmail.com>
2024-03-01 21:30:51 -0500

    Added board file, changes to CMakeLists.txt (not opening file)

--- headers/Board/VertexProps.h
+++ headers/Board/VertexProps.h
@@ -6,7 +6,7 @@ struct VertexProps
 {
 public:
 	const int NUM_NODES = 2;
-	const float NODE_SIZE = 30;
+	const float NODE_SIZE = 15;
 	const int NODE_OUTLINE_THICKNESS = 3;
 	const sf::Color NODE_FILL_COLOR = sf::Color::Green;
 	const sf::Color NODE_FILL_HOVER_COLOR = sf::Color::Blue;
